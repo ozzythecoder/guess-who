8c6f1d0f5155b59ee878f03b5fbfd89be1055c86
August McAllister <august.chm@gmail.com>
2022-10-31 17:55:43 -0500

    Implemented incorrect guess logic

--- client.js
+++ client.js
@@ -1,10 +1,12 @@
 $( document ).ready(onReady);
+let correctGuess;
 
 function onReady() {
   renderProfilePictures();
-
-  let correctGuess = getRandomPerson() // choose a random person's name
-  gameInit(correctGuess);
+  
+  correctGuess = getRandomPerson(); // set a random person's name as global correct value
+  console.log(correctGuess);
+  gameInit();
 
   $( '#pictures' ).on('click', 'img', evaluateGuess)
 }
@@ -12,26 +14,51 @@ function onReady() {
 function renderProfilePictures() {
   people.forEach( (person) => {
     $('#pictures').append(`
-    <img src="https://github.com/${person.githubUsername}.png?size=250" data-person="${person.name} alt="Profile image of ${person.name}">
+    <img src="https://github.com/${person.githubUsername}.png?size=250" data-person="${person.name}" alt="Profile image of ${person.name}">
     `)
   })
 }
 
-function gameInit(person) {
+function gameInit() {
   // prompt the player to click a picture
   $('#feedback').append(`
-    <p>Click on the picture of ${person.name}:</p>
+    <p>Click on the picture of ${correctGuess.name}:</p>
   `)
 }
 
 function evaluateGuess() {
-  
+
+  // get data of person who was clicked
+  let thisGuess = $( this ).data().person;
+
+  console.log('in evaluateGuess');
+  console.log('correct person is', correctGuess.name);
+  console.log('person clicked on is', thisGuess);
+
+  if (thisGuess == correctGuess.name) { 
+    gameWin();
+  } else {
+    gameLoss();
+  }
+}
+
+function gameWin() {
+  console.log('correct guess!');
+}
+
+function gameLoss() {
+  console.log('oops! learn your teacher\'s names!');
+  $( '#feedback' ).empty();
+  $( '#feedback' ).append(`
+    <p>Wrong! Try again.</p>
+  `);
+  gameInit();
 }
 
 function randomNumber(min, max){
     return Math.floor(Math.random() * (1 + max - min) + min);
 }
 
-const getRandomPerson = () => {
+function getRandomPerson() {
   return people[randomNumber(1, people.length) - 1]
 }
