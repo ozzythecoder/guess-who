a4cd0b781fc1f0286b5687bfd438abbcaa3318d1
August McAllister <august.chm@gmail.com>
2022-10-31 17:39:16 -0500

    Began game logic

--- client.js
+++ client.js
@@ -1,15 +1,31 @@
 $( document ).ready(onReady);
 
 function onReady() {
-  showProfilePictures();
+  renderProfilePictures();
+
+  let correctGuess = getRandomPerson() // choose a random person's name
+  gameInit(correctGuess);
+
+  $( '#pictures' ).on('click', 'img', evaluateGuess)
 }
 
-function showProfilePictures() {
+function renderProfilePictures() {
   people.forEach( (person) => {
     $('#pictures').append(`
-    <img src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}">
+    <img src="https://github.com/${person.githubUsername}.png?size=250" data-person="${person.name} alt="Profile image of ${person.name}">
     `)
   })
+}
+
+function gameInit(person) {
+  // prompt the player to click a picture
+  $('#feedback').append(`
+    <p>Click on the picture of ${person.name}:</p>
+  `)
+}
+
+function evaluateGuess() {
+  
 }
 
 function randomNumber(min, max){
