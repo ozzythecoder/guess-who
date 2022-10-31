fdf7de57582ca39ca763df26e0f39d8b34ac42af
August McAllister <august.chm@gmail.com>
2022-10-31 17:15:05 -0500

    Added jQuery, html structuring

--- client.js
+++ client.js
@@ -1,1 +1,13 @@
+$( document ).ready(onReady);
+
+function onReady() {
+  showProfilePictures();
+}
+
+// function showProfilePictures() {
+//   people.forEach( (person) => {
+
+//   })
+// }
+
 console.log('Here are all the available people:', people);
